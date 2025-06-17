88f039f822872bb8d4dd423cf11fe73d77d3da0b
kth <dms304@outlook.kr>
2025-06-18 03:04:35 +0900

    도커 안쓰게 변경

--- app/admin/MenuList.tsx
+++ app/admin/MenuList.tsx
@@ -1,8 +1,6 @@
 "use client";
-
-
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 
 const menus = ["주문/배송 조회", "상품 등록"];
 //const menuList = ["주문/배송 조회", "개인정보/배송지 수정", "포인트", "쿠폰"];
@@ -15,8 +13,14 @@ menuMap.set("상품 등록", "item");
 export default () => {
   //const [activeMenuIndex, setActiveMenuIndex] = useState<number>(0);
   //const [activeMenu, setActiveMenu] = useState<string>(menus[0]);
-  const params = useParams<{ menu: string }>();
-  const paramsMenu = params.menu;
+  //const params = useParams<{ menu: string }>();
+  //const paramsMenu = params.menu;
+
+  const path = usePathname(); // "/admin/item"
+  const segments = path.split("/"); // ["", "admin", "item"]
+  const currentMenu = segments[2]; // "item"
+
+
   return (
     <aside className=" border p-4">
       <div className="font-bold text-3xl py-3">관리자 페이지</div>
@@ -25,7 +29,7 @@ export default () => {
           <Link href={`/admin/${menuMap.get(menu)!}`} key={`menu-${index}`}>
             <li
               className={`p-1 cursor-pointer ${
-                paramsMenu === menuMap.get(menu) ? "font-bold" : ""
+                currentMenu === menuMap.get(menu) ? "font-bold" : ""
               }`} // 클릭된 메뉴에 font-bold 적용
             >
               {menu}
