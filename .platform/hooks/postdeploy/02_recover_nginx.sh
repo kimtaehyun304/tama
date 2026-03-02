a286fddf21e8150ad5af6b7b178e2a35c2f8604e
kth <dms304@outlook.kr>
2026-03-02 23:15:43 +0900

    스크립트 로그 메시지 수정

--- .platform/hooks/postdeploy/02_recover_nginx.sh
+++ .platform/hooks/postdeploy/02_recover_nginx.sh
@@ -2,7 +2,7 @@
 
 # systemd가 nginx를 자동으로 시작하려는데, 이미 nignx가 떠있는 경우 때문에
 if  systemctl is-active --quiet nginx; then
-    echo "[INFO] nginx is active"
+    echo "[INFO] nginx is active. so skip to restart nginx"
 else 
     echo "[WARN] nginx is not active. try to start nginx"
     sudo pkill -f nginx
